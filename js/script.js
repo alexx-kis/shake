afd2f7c329a9aa92f42d7f176aa29832e562263a
alexx-kise <kislov@advline.rum>
2023-10-20 10:57:59 +0300

    modified

--- js/script.js
+++ js/script.js
@@ -3,29 +3,35 @@
 let square = document.querySelector('.square');
 
 
-let myShakeEvent = new Shake({
-	// threshold: 15, // optional shake strength threshold
-	// timeout: 1000 // optional, determines the frequency of event generation
-});
+// let myShakeEvent = new Shake({
+// 	// threshold: 15, // optional shake strength threshold
+// 	// timeout: 1000 // optional, determines the frequency of event generation
+// });
 
-myShakeEvent.start();
+// myShakeEvent.start();
 
-window.addEventListener('shake', shakeEventDidOccur, false);
+// window.addEventListener('shake', shakeEventDidOccur, false);
 
-//function to call when shake occurs
-function shakeEventDidOccur() {
+// //function to call when shake occurs
+// function shakeEventDidOccur() {
 
-	//put your own code here etc.
-	alert('shake!');
+// 	//put your own code here etc.
+// 	alert('shake!');
+
+// 	square.classList.add('square--active');
+
+
+// }
+
+
+function handleMotionEvent(event) {
+	const x = event.accelerationIncludingGravity.x;
+	const y = event.accelerationIncludingGravity.y;
+	const z = event.accelerationIncludingGravity.z;
+
+	// Do something awesome.
 
 	square.classList.add('square--active');
-
-
 }
-
 
 window.addEventListener("devicemotion", handleMotionEvent, true);
-
-function handleMotionEvent() {
-	square.classList.add('square--active');
-}
