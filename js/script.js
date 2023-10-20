65aba4d1b88159ab1963c9d28be14a74b7d1ef5e
alexx-kise <kislov@advline.rum>
2023-10-20 10:19:02 +0300

    feat: shake event added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+let square = document.querySelector('.square');
+
+
+let myShakeEvent = new Shake({
+	threshold: 15, // optional shake strength threshold
+	timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur() {
+
+	//put your own code here etc.
+	alert('shake!');
+
+	square.classList.add('square--active');
+
+
+}
